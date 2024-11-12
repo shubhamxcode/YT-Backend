40d231acf4a6d910fe1a3b8bc248223ceda9a97d
shubhamxcode <shubh.varshneycode@gmail.com>
2024-11-13 02:11:08 +0530

    api error handling

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,19 @@
+// const asynchandler=(fn)=>async (req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })     
+//     }
+
+// }
+const asynchandler=(requesthandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requesthandler(req,res,next))
+        .catch((error)=>next(error)) 
+    }  
+}
+
+export default asynchandler

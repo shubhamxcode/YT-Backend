b0819b0e803139cb4e9885609b1477b4cb282ad3
shubhamxcode <shubh.varshneycode@gmail.com>
2024-11-19 11:37:57 +0530

    registration of user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,21 @@
 import asynchandler from '../utils/asynchandler.js'
-
+import Apierror from '../utils/Apierrors.js'
 const registeruser=asynchandler(async(req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+    //get user details from frontend 
+    //validation-not empty
+    //check if user already exit :username,email,
+    //check for images and avtar  
+    //upload them in to cloudinary,avtar
+    //create user object-create entry in db  
+    //remove password and refresh token field from response 
+    //check for user creation
+    //return response  
+
+    const {username,fullname,password,email}=req.body
+    console.log("password:",email);
+    if ([username,fullname,password,email].some((field)=>field?.trim()==="")) {
+        throw new Apierror(400,"all field are required ")
+    } 
 }) 
 
 export default registeruser
